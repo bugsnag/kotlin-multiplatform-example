49b89f7188a34a90571041d01123d1518a9d3389
Michał Ciołek <michalciolek@gmail.com>
2019-08-21 14:42:52 +0200

    Update libraries and fix building

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("com.android.tools.build:gradle:${Versions.agp}")
         classpath("com.bugsnag:bugsnag-android-gradle-plugin:${Versions.bugsnagPlugin}")
-        classpath("org.jetbrains.kotlin:kotlin-frontend-plugin:0.0.37")
+        classpath("org.jetbrains.kotlin:kotlin-frontend-plugin:${Versions.kotlinFrontentPlugin}")
     }
 }
 
@@ -22,5 +22,19 @@ allprojects {
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
         maven("https://dl.bintray.com/kotlin/kotlin-js-wrappers")
         maven("https://kotlin.bintray.com/kotlinx")
+    }
+
+    // TODO remove once https://github.com/Kotlin/kotlin-frontend-plugin/issues/141 is fixed
+    plugins.whenPluginAdded {
+        if (this.javaClass.name == "org.jetbrains.kotlin.gradle.frontend.FrontendPlugin") {
+            val fixTask = tasks.register("webpack-config-fix") {
+                this.doLast { file("webpack.config.d").mkdir() }
+            }
+            afterEvaluate {
+                tasks.named("webpack-config").configure {
+                    this.dependsOn(fixTask)
+                }
+            }
+        }
     }
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,10 +1,14 @@
 object Versions {
-    val kotlin = "1.3.20"
-    val agp = "3.2.1"
+    val kotlin = "1.3.41"
+    val kotlinxHtml = "0.6.12"
+    val kotlinReact = "16.9.0-pre.82-kotlin-1.3.41"
+    val nodeJs = "12.9.0"
+    val agp = "3.4.2"
     val androidx = "1.0.0"
     val bugsnagAndroid = "4.11.0"
     val bugsnagPlugin = "4.0.0"
     val bugsnagJvm = "3.4.4"
-    val springBoot = "2.1.1.RELEASE"
-    val depManagement = "1.0.6.RELEASE"
+    val kotlinFrontentPlugin = "0.0.45"
+    val springBoot = "2.1.7.RELEASE"
+    val depManagement = "1.0.8.RELEASE"
 }
